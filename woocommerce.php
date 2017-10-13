35c4d149765f8673ca6ff831d8d87a8ad06ffdc3
Jay <jaycbrf@gmail.com>
2017-10-13 11:07:28 -0400

    update woocommerce.php

--- woocommerce.php
+++ woocommerce.php
@@ -1,17 +1,16 @@
 <?php get_header(); ?>
 
    <div id="content">
-   <div  class="container">
-   <div class="row">
-        <div id="main" class="clearfix col-xs-12" role="main">
+     <div  class="container">
+       <div class="row">
+          <div id="main" class="clearfix col-xs-12" role="main">
 
-          <?php woocommerce_content(); ?>
-      
-        </div> <!-- /main -->
+            <?php woocommerce_content(); ?>
+        
+          </div> <!-- /main -->
         </div><!--/row-->
-
       </div><!-- /container-->    
-      </div> <!-- /content -->
+    </div> <!-- /content -->
 
 
 <?php get_footer(); ?>
