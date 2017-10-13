ad59ea21a9cd194666d345a08dbf63fc7ce29525
Jay <jaycbrf@gmail.com>
2017-10-13 10:48:54 -0400

    update page-standard-sidebar.php

--- page-standard-sidebar.php
+++ page-standard-sidebar.php
@@ -1,47 +1,42 @@
 <?php
-/* Template Name: Sidebar-Page
+/* Template Name: Sidebar-Page Full Width
 */
 ?>
 
 <?php get_header(); ?>
 
-<div id="content">
-   <div  class="container-fluid">
-   <div class="row">
-      <div id="main" class="clearfix col-sm-8 col-md-9" role="main">
+      <div id="content">
+        <div  class="container-fluid">
+          <div class="row">
+              <div id="main" class="clearfix col-sm-8 col-md-9" role="main">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          
-          <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-            
+                <section class="post_content">
+                  <?php the_content(); ?>
+                </section> <!-- end article section -->
 
-            <section class="post_content">
-              <?php the_content(); ?>
-          
-            </section> <!-- end article section -->
-          
-          </article> <!-- end article -->
-          
-          <?php endwhile; ?>  
-          
-          <?php else : ?>
-          
-          <article id="post-not-found">
-              <header>
-                <h1><?php _e("Not Found", "JD_BOOTSTRAP"); ?></h1>
-              </header>
-              <section class="post_content">
-                <p><?php _e("Sorry, but the requested resource was not found on this site.", "JD_BOOTSTRAP"); ?></p>
-              </section>
-             
-          </article>
-          
-          <?php endif; ?>
+                </article> <!-- end article -->
 
-      </div><!--/main-->
-              <?php get_sidebar(); // sidebar 1 ?>
-      </div><!--row-->
-</div><!--/container-->
-   </div><!-- /. content -->
+                <?php endwhile; ?>  
+
+                <?php else : ?>
+
+                <article id="post-not-found">
+                  <header>
+                    <h1><?php _e("Not Found", "JD_BOOTSTRAP"); ?></h1>
+                  </header>
+                  <section class="post_content">
+                   <p><?php _e("Sorry, but the requested resource was not found on this site.", "JD_BOOTSTRAP"); ?></p>
+                  </section>
+                </article>
+
+                <?php endif; ?>
+
+              </div><!--/main-->
+            <?php get_sidebar(); // sidebar 1 ?>
+          </div><!--row-->
+        </div><!--/container-->
+      </div><!-- /. content -->
 <?php get_footer(); ?>
 
