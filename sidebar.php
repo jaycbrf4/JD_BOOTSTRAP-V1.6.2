363e80b964dba3d2c443d9175dc891f94322113e
Jay <jaycbrf@gmail.com>
2017-10-20 10:17:05 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,14 @@
-	<div id="sidebar1" class="col-sm-4 col-md-3" role="complementary">
-		<aside>		
+<div id="sidebar1" class="col-sm-4 col-md-3" role="complementary">
+	<aside>		
 		<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-
-			<?php dynamic_sidebar( 'sidebar1' ); ?>
+		<?php dynamic_sidebar( 'sidebar1' ); ?>
 
 		<?php else : ?>
-		</aside>
-			<!-- This content shows up if there are no widgets defined in the backend. -->
-						
-				<div class="alert alert-message">
-						
-					<p><?php _e("Please activate some Widgets","JD_BOOTSTRAP"); ?>.</p>
-						
-				</div>
-
-		<?php endif; ?>
-
+	</aside>
+		<!-- This content shows up if there are no widgets defined in the backend. -->	
+	<div class="alert alert-message">						
+		<p><?php _e("No sidebar widgets active","JD_BOOTSTRAP"); ?>.</p>						
 	</div>
+	<?php endif; ?>
+</div><!--/.sidebar -->
 
