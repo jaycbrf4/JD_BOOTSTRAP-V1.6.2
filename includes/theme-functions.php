a5689b6a90688ad230eef2b32941770bceb978f6
Jay <jaycbrf@gmail.com>
2017-10-26 15:01:07 -0400

    Update theme-functions.php
    remove woo support by default and place in include

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -180,30 +180,6 @@ add_filter('the_content', 'filter_ptags_on_images');
 add_filter('tiny_mce_before_init', 'my_change_mce_options');
 
 /*
-*
- ADD WOO Support
-*
-*/
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-  function my_theme_wrapper_start() {
-    echo '<section class="clearfix">';
-  }
-
-  function my_theme_wrapper_end() {
-    echo '</section>';
-  }
-
-add_action( 'after_setup_theme', 'woocommerce_support' );
-  function woocommerce_support() {
-      add_theme_support( 'woocommerce' );
-  }
-
-/*
 ** Bootstrap Responsive Images 
 */
 // remove width and height from images
